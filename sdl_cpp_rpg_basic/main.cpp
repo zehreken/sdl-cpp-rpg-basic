bfa46f13da78638de4858cd4d0192be178988cbf
zehreken <zehreken@gmail.com>
2019-05-03 20:35:55 +0200

    Add initial files

---
+++ sdl_cpp_rpg_basic/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  sdl_cpp_rpg_basic
+//
+//  Created by Guchan Alkan on 3.05.2019.
+//  Copyright © 2019 zehreken. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
