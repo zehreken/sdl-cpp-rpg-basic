5c64d71df20245abf1489d3d43c68fc1ef74079b
zehreken <zehreken@gmail.com>
2019-05-22 20:12:50 +0200

    Add optimization, does not seem to have any effect though

--- sdl_cpp_rpg_basic/gameObject/view.cpp
+++ sdl_cpp_rpg_basic/gameObject/view.cpp
@@ -20,6 +20,8 @@ void View::render(SDL_Renderer *p_renderer)
 	pos.x -= cameraPos.x;
 	pos.y -= cameraPos.y;
 //	std::cout << pos.x << " " << pos.y << "\n";
+	
+//	if (pos.x < 960 && pos.y < 540) // Optimization, does not seem to effect?
 	_p_gameObject->p_spriteSheet()->render(p_renderer, pos.x, pos.y, &clipRect);
 }
 
